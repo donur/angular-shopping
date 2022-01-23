e5b08abfe08a9089296ee81ca78490d0fa424a86
Onur Dogan <iamonurdogan@gmail.com>
2022-01-23 11:48:19 +0300

    Json server yapılandırıldı. Http metotları hazırlandı

---
+++ data.js
@@ -0,0 +1,29 @@
+module.exports = function() {
+  return {
+    products: [
+        { id: 1, name: "Napoli Pizza", price: 35, imageUrl: '1.png', description: 'Italyan Pizza' },
+        { id: 2, name: "Lahmacun", price: 24, imageUrl: '2.png', description: 'Fırın Yemeği' },
+        { id: 3, name: "Adana Kebap", price: 40, imageUrl: '3.png', description: 'Izgara' },
+        { id: 4, name: "Urfa Kebap", price: 45, imageUrl: '4.png', description: 'Izgara' },
+        { id: 5, name: "Kır Pidesi", price: 25, imageUrl: '5.png', description: 'Fırın Yemeği' },
+        { id: 6, name: "Kaşarlı Pide", price: 35, imageUrl: '6.png', description: 'Fırın Yemeği' },
+        { id: 7, name: "Kuşbaşılı Pide", price: 40, imageUrl: '7.png', description: 'Fırın Yemeği' },
+        { id: 8, name: "Mercimek Çorbası", price: 15, imageUrl: '8.png', description: 'Çorba Çeşitleri' },
+        { id: 9, name: "Ezogelin Çorbası", price: 15, imageUrl: '9.png', description: 'Çorba Çeşitleri' },
+        { id: 10, name: "Mantı", price: 35, imageUrl: '10.png', description: 'Hamur Yemekleri' },
+        { id: 11, name: "Yaprak Sarma", price: 25, imageUrl: '11.png', description: 'Zytinyağlılar' }
+    ],
+    categories: [
+        {id: 1, name: "Ana Yemekler"},
+        {id: 2, name: "Fırın Yemekleri"},
+        {id: 3, name: "ızgaralar"},
+        {id: 4, name: "Çorbalar"},
+        {id: 5, name: "Tatlılar"},
+        {id: 6, name: "İçecekler"}
+
+    ],
+    orders: [
+
+    ]
+  }
+}
